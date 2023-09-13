2eeb961ad1d54cb9f5d7ae7d7ab5dedf04c6c535
elifred2022 <elifed21@hotmail.com>
2023-09-13 19:58:18 -0300

    operadores numerales y aritemticos

--- jsintenso.js
+++ jsintenso.js
@@ -1,3 +1,28 @@
-// https://www.youtube.com/watch?v=z95mZVUcJ-E&t=676s 42:20
+// https://www.youtube.com/watch?v=z95mZVUcJ-E&t=676s 1:00:00
 
-alert("hola");
+// variables: declarar; let, var, const (numericos, string, buleanos)
+// casos especiales; undefined, null, nan, symbol
+
+let nombre = prompt("nombre"); // prompt para ingresar datos el usuario
+
+alert("hola" + nombre);
+
+// operadores numericos
+
+// operado de asigancion numero ++ -- // ** = 5
+// resto; numero %= 5 es el remanente exacto de la division
+// exponenciacion numero **= 5
+
+let numero = 10;
+
+numero += 5;
+
+document.write(numero);
+
+// operadores aritmeticos: numero + - / * = 5
+
+let numero1 = 10,
+  nuemro2 = 5;
+
+let resultado = numero1 + nuemro2;
+alert(resultado);
